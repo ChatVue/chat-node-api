8dec01fdc5d1dddda42d1c1dda7c345aee58a7eb
user4i <trovinba@gmail.com>
2019-03-03 12:38:18 +0200

    load history and count limit

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     mongoURI: 'mongodb://localhost:27017/chat',
     port: '5009',
-    tokenKey: 'Our Token Key'
+    tokenKey: 'Our Token Key',
+
+    // max count of loaded messages per time,
+    // messages with same time (ms) will be added over.
+    loadCountLimit: 20
 };
--- routes/messages.js
+++ routes/messages.js
@@ -1,12 +1,26 @@
 const router = require('express').Router();
-const mongoose = require('mongoose');
+const config = require('../config');
 
 const Message = require('../models/message');
 
-router.get('/messages', (req, res) => {
-    Message.find({}).populate('author', 'nick').exec(function(err, messages) {
-        return res.json({ messages: messages });
-    });
+router.get('/messages', async (req, res) => {
+    const filter = {};
+    const count =
+        req.query.count && parseInt(req.query.count) > 0 ? parseInt(req.query.count) : config.loadCountLimit || 20;
+
+    if (req.query.dateBefore) {
+        filter.createdAt = { $lt: req.query.dateBefore };
+    }
+
+    // Get first message from count and over add messages with the same time
+    const firstMsg = await Message.findOne(filter).sort({ createdAt: -1 }).skip(count - 1).limit(1).exec();
+    if (firstMsg) {
+        filter.createdAt = filter.createdAt || {};
+        filter.createdAt.$gte = firstMsg.createdAt;
+    }
+
+    const messages = await Message.find(filter).populate('author', 'nick').sort({ createdAt: 1 }).exec();
+    return res.json({ messages: messages });
 });
 
 module.exports = router;
