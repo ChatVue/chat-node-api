fd69537205e2e374996678a2c09707d546780948
user4i <trovinba@gmail.com>
2019-05-16 23:59:42 +0300

    refresh tokens in sockets

--- routes/socket.js
+++ routes/socket.js
@@ -5,40 +5,47 @@ const { parseBearer } = require('../utils/token');
 function run(server) {
     const io = require('socket.io')(server);
     io.on('connection', (client) => {
-        client.on('NEW', (bearer, msg, tmpId) => {
+        client.on('NEW', (bearer, msg, tmpId, retry) => {
             let userData = {};
             try {
                 userData = parseBearer(bearer, client.request.headers);
+                const message = new Message({
+                    _id: new mongoose.Types.ObjectId(),
+                    message: msg,
+                    author: userData.id
+                });
+                message
+                    .save()
+                    .then((result) => {
+                        Message.findOne({ _id: result._id }).populate('author', 'nick').exec(function(err, message) {
+                            // TODO: Broadcast only for logged in users
+                            client.broadcast.emit('ADD', message);
+                            client.emit('UPDATE', { message, tmpId });
+                        });
+                    })
+                    .catch();
             } catch (err) {
-                client.emit('LOGOUT');
+                if (!retry) {
+                    client.emit('REFRESH', [ 'NEW', bearer, msg, tmpId ]);
+                } else {
+                    client.emit('LOGOUT');
+                }
                 return;
             }
-            const message = new Message({
-                _id: new mongoose.Types.ObjectId(),
-                message: msg,
-                author: userData.id
-            });
-            message
-                .save()
-                .then((result) => {
-                    Message.findOne({ _id: result._id }).populate('author', 'nick').exec(function(err, message) {
-                        // TODO: Broadcast only for logged in users
-                        client.broadcast.emit('ADD', message);
-                        client.emit('UPDATE', { message, tmpId });
-                    });
-                })
-                .catch();
         });
-        client.on('TYPING', (bearer) => {
+        client.on('TYPING', (bearer, retry) => {
             let userData = {};
             try {
                 userData = parseBearer(bearer, client.request.headers);
+                client.broadcast.emit('TYPING', userData.nick);
             } catch (err) {
-                client.emit('LOGOUT');
+                if (!retry) {
+                    client.emit('REFRESH', [ 'TYPING', bearer ]);
+                } else {
+                    client.emit('LOGOUT');
+                }
                 return;
             }
-            // TODO: Broadcast only for logged in users
-            client.broadcast.emit('TYPING', userData.nick);
         });
     });
     return io;
